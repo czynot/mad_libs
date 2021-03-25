a1ece78ebf982c481d638c8a7587eb627c66f947
Parth Purvesh <parth29dec@gmail.com>
2021-03-25 17:04:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+colour = input("Enter a colour: ")
+plural_noun = input("Enter a plural noun: ")
+sportsman = input("Enter a sportsman: ")
+
+print("Roses are " + colour)
+print(plural_noun + " are blue")
+print("I love " + sportsman)
